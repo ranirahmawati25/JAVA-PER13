5f6dbba1c64f976ebeb5124b05f1ae4f64a685a4
ranirahmawati25 <ranirahmawati75664@gmail.com>
2019-12-12 22:52:34 +0700

    JAVA PERTEMUAN 13

---
+++ 04-Memahami Program/Main/src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * Nama     :Rani Rahmawati
+ * Kelas    :TI-B
+ * Nim      :A2.1900142
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
+   
---
+++ 05-Organisasi program dengan IDE/Program dengan metode manual/src/com/ucup/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ucup;
+
+/**
+ *
+ * @author willy
+ * Nama     :Rani Rahmawati
+ * Kelas    :TI-B
+ * Nim      :A2.1900142
+ */
+public class Main {
+    public static void main(String[] args){
+		System.out.println("hello world");
+	}
+}
